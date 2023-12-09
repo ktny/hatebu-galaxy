6a7a094a4df52609297b2aded25c2bf1c2d46969
ktny <katsunori.t10@gmail.com>
2023-12-09 16:10:26 +0900

    add Search

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Search from "./ui/search";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <Search></Search>
+    </main>
+  );
 }
---
+++ app/ui/search.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Search() {
+  const [username, setUsername] = useState("");
+
+  const disabled = username.length <= 2;
+
+  return (
+    <div className="flex">
+      <input
+        placeholder="username"
+        className="input input-bordered input-primary max-w-xs mr-2"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Link href={`/user/${username}`} style={{ pointerEvents: disabled ? "none" : "auto" }}>
+        <button className="btn btn-primary" disabled={disabled}>
+          Go
+        </button>
+      </Link>
+    </div>
+  );
+}
