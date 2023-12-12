dd161970d6b1319d4f93928731eb96822ba89467
ktny <katsunori.t10@gmail.com>
2023-12-12 23:28:43 +0900

    ブックマークの日付表示

--- app/ui/bookmark.tsx
+++ app/ui/bookmark.tsx
@@ -7,7 +7,9 @@ export default function Bookmark({ username, bookmark }: { username: string; boo
   return (
     <div className="card card-compact w-full shadow-xl mt-8">
       <div className="flex items-center gap-2">
-        <figure className="shrink-0 w-24 md:w-52">{bookmark.image && <Image src={bookmark.image} width={200} height={112} alt={bookmark.title} />}</figure>
+        <figure className="shrink-0 w-24 md:w-52">
+          {bookmark.image && <Image src={bookmark.image} width={200} height={112} alt={bookmark.title} />}
+        </figure>
         <h2 className="card-title block p-2">
           <Link href={bookmark.entryURL} target="_blank" className="mr-2">
             {bookmark.title}
@@ -17,14 +19,17 @@ export default function Bookmark({ username, bookmark }: { username: string; boo
           </Link>
         </h2>
       </div>
-      <div className="card-body">
-        <p className="text-lg">
-          <Link href={`https://b.hatena.ne.jp/entry/${bookmark.eid}/comment/${username}`} target="_blank">
-            {bookmark.comment}
-          </Link>
-        </p>
-        <StarList starsCount={bookmark.star} displayIfZero={false}></StarList>
-      </div>
+      <Link
+        className="card-body"
+        href={`https://b.hatena.ne.jp/entry/${bookmark.eid}/comment/${username}`}
+        target="_blank"
+      >
+        <p className="text-lg">{bookmark.comment}</p>
+        <div className="flex justify-between">
+          <StarList starsCount={bookmark.star} displayIfZero={false}></StarList>
+          <span className="shrink-0">{bookmark.bookmarkDate}</span>
+        </div>
+      </Link>
     </div>
   );
 }
