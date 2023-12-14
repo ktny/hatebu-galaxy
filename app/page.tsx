83835e8ea45f16ec7bcc87d354e70cdb788a5cf3
ktny <katsunori.t10@gmail.com>
2023-12-14 22:29:11 +0900

    トップページのUI調整

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Search from "./ui/search";
 
 export default function Home() {
   return (
-    <main className="h-screen flex justify-center items-center">
-      <Search></Search>
+    <main className="flex justify-center mt-8">
+      <div className="flex flex-col items-center gap-8">
+        <p className="text-2xl">はてなブックマーカーの星を観に行く</p>
+        <Search></Search>
+      </div>
     </main>
   );
 }
