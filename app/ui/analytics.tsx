4d10835a62c3bd860e6529729bad818a21c935a3
ktny <katsunori.t10@gmail.com>
2023-12-17 19:11:12 +0900

    fix

--- app/ui/analytics.tsx
+++ app/ui/analytics.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 import Script from "next/script";
+import { usePathname } from "next/navigation";
 
 export default function GoogleAnalytics() {
+  const pathname = usePathname();
   const GA_MEASUREMENT_ID = "G-NMTLSQVC0B";
 
   return (
-    <>
+    <div>
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', ${GA_MEASUREMENT_ID});
+          gtag('config', ${GA_MEASUREMENT_ID}, {
+            page_path: ${pathname},
+          });
         `}
       </Script>
-    </>
+    </div>
   );
 }
