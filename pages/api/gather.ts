fc436992f1a8712a5869e1aca33edd0331ccd731
ktny <katsunori.t10@gmail.com>
2024-01-01 21:10:09 +0900

    fix

--- pages/api/gather.ts
+++ pages/api/gather.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if (data.bookmarks.length === 0) {
     res.status(404).json({});
+  } else {
+    res.status(200).json(data);
   }
-
-  res.status(200).json(data);
 }
