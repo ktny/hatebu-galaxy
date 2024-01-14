f403c8d59bcecc8c0704e0fc7c81bf524945b12f
ktny <katsunori.t10@gmail.com>
2024-01-14 18:40:42 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: true,
-  swcMinify: false,
+  // productionBrowserSourceMaps: true,
+  // swcMinify: false,
   reactStrictMode: false, // 開発環境でAPIが2回呼ばれるのを防止
   images: {
     remotePatterns: [
